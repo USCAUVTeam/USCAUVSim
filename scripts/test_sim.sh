cb904232a8189cd4ad954d18f1b10548a752200d
mantaraylab1 <mantaraylab1@gmail.com>
2023-09-06 21:35:08 -0700

    Added a scripts folder with some init scripts for docker image

---
+++ scripts/test_sim.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+roslaunch uuv_gazebo_worlds empty_underwater_world.launch
+roslaunch mantaray_description upload.launch
+rostopic pub -1 /mantaray/thrusters/0/input uuv_gazebo_ros_plugins_msgs/FloatStamped -- '
+{
+header: {seq: 13000, stamp: now},
+data: 100
+}'
