4af0cd29cdaacf8764ba1231d63007d69e909219
mantaraylab1 <mantaraylab1@gmail.com>
2023-09-13 17:33:32 -0700

    Gave the sub more weight to make it less bouyant

--- scripts/test_sim.sh
+++ scripts/test_sim.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-roslaunch uuv_gazebo_worlds empty_underwater_world.launch
-roslaunch mantaray_description upload.launch
+roslaunch uuv_gazebo_worlds empty_underwater_world.launch&
+sleep 10
+roslaunch mantaray_description upload.launch&
+sleep 5
 rostopic pub -1 /mantaray/thrusters/0/input uuv_gazebo_ros_plugins_msgs/FloatStamped -- '
 {
 header: {seq: 13000, stamp: now},
